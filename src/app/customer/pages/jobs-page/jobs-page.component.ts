5005c572a3aec0c970378346ca74e8875f8945d4
Austin Houck <akh4rf@virginia.edu>
2021-08-06 02:50:51 -0400

    Finished Jobs Page Draft

--- src/app/customer/pages/jobs-page/jobs-page.component.ts
+++ src/app/customer/pages/jobs-page/jobs-page.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HTTPRequestService } from 'src/app/shared/services/http-request.service';
-import { Location } from '../../_models/location';
 
 @Component({
   selector: 'jobs-page',
@@ -8,21 +6,16 @@ import { Location } from '../../_models/location';
   styleUrls: ['./jobs-page.component.css'],
 })
 export class JobsPageComponent implements OnInit {
-  LOCATIONS!: Array<Location>;
-  /**
-   * A method to retrieve the list of locations from backend
-   * @returns
-   */
-  getLocations() {
-    return this.httpRequest
-      .get(this.httpRequest.getPHPBaseURL() + 'locations/all-locations.php')
-      .subscribe((locationData) => {
-        this.LOCATIONS = locationData;
-      });
-  }
-  constructor(private httpRequest: HTTPRequestService) {}
+  JOBS = [
+    'Morning Cook',
+    'Evening Cook',
+    'Dishwasher',
+    'Sandwich Maker',
+    'Cashier',
+    'Assistant Manager',
+    'Manager',
+  ];
+  constructor() {}
 
-  ngOnInit(): void {
-    this.getLocations();
-  }
+  ngOnInit(): void {}
 }
