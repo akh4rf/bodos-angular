d4d16f811ad7912237ce00b93043c1fa3cd4c37d
Austin Houck <akh4rf@virginia.edu>
2021-08-09 23:33:42 -0400

    Added timeline to Our Story Page

--- src/app/customer/pages/our-story-page/our-story-page.component.ts
+++ src/app/customer/pages/our-story-page/our-story-page.component.ts
@@ -6,6 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./our-story-page.component.css'],
 })
 export class OurStoryPageComponent implements OnInit {
+  TIMELINE = [
+    {
+      date: 1988,
+      text: "The first Bodo's Bagels opens on Emmet Street.",
+    },
+    {
+      date: 1993,
+      text: "Preston Avenue Bodo's opens.",
+    },
+    {
+      date: 2005,
+      text: "Bodo's comes to The Corner, and students rejoice.",
+    },
+  ];
+
   constructor() {}
 
   ngOnInit(): void {}
